37b4adbd46fbd7410c2647c26c840099728fbfe2
Jinyoung Jang <jjno7@naver.com>
2017-02-20 11:22:04 +0900

    매칭 상태 추가

--- routes/match.js
+++ routes/match.js
@@ -3,6 +3,7 @@
  */
 var express = require('express');
 var mysql = require('mysql');
+var waterfall = require("async/waterfall");
 var router = express.Router();
 
 var pool = mysql.createPool({
@@ -18,7 +19,9 @@ function getMatch(req, res, next) {
     var user_id = req.query.user_id;
     var matchList = [];
 
-    pool.query('SELECT o.id AS order_id, o.status AS status, s.name AS store_name, m.name AS main_menu_name, m.image_url AS main_menu_image_url ' +
+    pool.query('SELECT o.id AS order_id, s.id AS store_id, m.id AS main_menu_id, o.status AS status, ' +
+                's.name AS store_name, m.name AS main_menu_name, ' +
+                'm.image_url AS main_menu_image_url, m.require_people_num AS require_people_num ' +
                 'FROM orders o JOIN stores s ON o.store_id = s.id ' +
                             'JOIN menu m ON o.main_menu_id = m.id ' +
                 'WHERE o.user_id = ? '+
@@ -28,9 +31,12 @@ function getMatch(req, res, next) {
                 var item = {
                     'message' : 'success',
                     'order_id' : row.order_id,
+                    'store_id' : row.store_id,
+                    'main_menu_id' : row.main_menu_id,
                     'status' : row.status,
                     'store_name' : row.store_name,
                     'main_menu_name' : row.main_menu_name,
+                    'require_people_num' : row.require_people_num,
                     'main_menu_image_url' : row.main_menu_image_url
                 };
                 matchList.push(item)
@@ -45,17 +51,20 @@ function getMatch(req, res, next) {
 
 function getExtraManus(req, res, next) {
     var order_id = req.query.order_id;
+    var store_id = req.query.store_id;
+    var main_menu_id = req.query.main_menu_id;
     var extraList = [];
-    pool.query('SELECT name AS extra_name, count AS extra_count, price AS extra_price '+ 
-                'FROM ordered_menu om JOIN menu m ON om.menu_id = m.id '+ 
-                'WHERE om.order_id = ? AND m.type = "extra"', [order_id], 
+    waterfall([
+        function(callback){
+            pool.query('SELECT name AS extra_name, count AS extra_count, price AS extra_price '+ 
+                    'FROM ordered_menu om JOIN menu m ON om.menu_id = m.id '+ 
+                    'WHERE om.order_id = ? AND m.type = "extra"', [order_id], 
                 function (err, rows) {
                     if (!err) {
                         if (rows.length != 0) {
                             var extraList = [];
                             rows.forEach(function (row) {
                                 var extraItem = {
-                                    'message' : 'success',
                                     'extra_menu_name': row.extra_name,
                                     'extra_menu_count': row.extra_count,
                                     'extra_menu_price': row.extra_price
@@ -63,15 +72,69 @@ function getExtraManus(req, res, next) {
                                 extraList.push(extraItem);
                                 console.log(extraList)
                             });
-                            res.json(extraList)
+                            callback(null, pool, order_id, extraList);
                         } else {
-                            res.json({'message' : 'noRow'})
+                            res.json({'message' : 'success'})
                         }
                     } else {
                         res.json({'message': 'fail'});
                     }
                 });
-        return extraList
+        },
+
+        function(pool, order_id, extraList, callback) {
+                var result;
+                // 1. 지금 등록한 주문을 제외한 매칭가능한 주문 수 조회
+                pool.query('SELECT o.id as matchOrders, o.user_id as user_id, m.require_people_num as require_people_num '+
+                    'FROM orders o JOIN menu m ON o.main_menu_id = m.id '+
+                    'WHERE o.status="waiting" AND o.store_id=? AND o.main_menu_id=? AND o.id!=? '+
+                    'ORDER BY o.expire_time ASC ',
+                    [store_id, main_menu_id, order_id],
+                    function (err, rows) {
+                        if (!err) {
+                            console.log(rows.length+" "+rows);
+                            // 2. 현재 주문 수보다 충족 수가 작을 경우 처리
+                            if (rows.length == 0) {
+                                result = {
+                                    'message':'success',
+                                    'status': 'waiting',
+                                    'current_people_num': 1,
+                                    'extra_menu' : extraList
+                                };
+                            }
+                            else if ( (rows.length + 1) < rows[0].require_people_num ) {
+                                result = {
+                                    'message':'success',
+                                    'status': 'waiting',
+                                    'current_people_num': rows.length,
+                                    'extra_menu' : extraList
+                                };
+                            }
+                            // 3. 현재 주문 수가 충족 수를 만족했을 때 처리
+                            else {
+                                result = {
+                                    'message':'success',
+                                    'status' : 'match',
+                                    'current_people_num': rows.length,
+                                    'extra_menu' : extraList
+                                };
+                            }
+                            callback(null, result);
+                        } else {
+                            res.json({'message': 'fail'});
+                            console.log('Error while performing Query.', err);
+                        }
+                    });
+        }],
+        function(err, result){
+            if(err) {
+                console.log("err: "+err);
+            } else {
+                console.log(result);
+                res.json(result);
+            }
+        }
+    );
 }
 router.get('/', getMatch);
 router.get('/extra', getExtraManus);
