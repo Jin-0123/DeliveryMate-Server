6ead3e39e05658d06f79cfd0feb6c2d3eba424ac
Jinyoung Jang <jjno7@naver.com>
2017-02-21 14:50:22 +0900

    매장이미지 추가

--- routes/stores.js
+++ routes/stores.js
@@ -36,7 +36,7 @@ function getStores(req, res, next) {
             res.json(stores);
             console.log(stores);
         } else {
-            res.json({"message": "fail"});
+            res.json({'message': 'fail'});
             console.log('Error while performing Query.', err);
         }
     });
